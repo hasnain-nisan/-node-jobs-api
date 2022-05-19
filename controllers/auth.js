e43c12b79ba5d0c2814357eb402183e65bb0bb99
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-05-19 14:07:51 -0700

    new user created

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,12 @@
 
-const register = async (req, res) => {
-    res.send('Register User')
-}
+const User = require('../models/user')
+const {StatusCodes} = require('http-status-codes')
 
+const register = async (req, res) => {
+  const user = await User.create(req.body);
+    res.status(StatusCodes.CREATED).send(user)
+}
+ 
 const login = async (req, res) => {
   res.send("Login User");
 };
---
+++ models/User.js
@@ -0,0 +1,27 @@
+
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    minlength: 3,
+    maxlength: 50,
+  },  
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    match: [
+      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+      "Please provide a valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide an password"],
+    minlength: 6,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema)
