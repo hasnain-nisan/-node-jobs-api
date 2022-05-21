18b57861d23a23acd6b6a1acbf7859c0675f9fff
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-05-21 16:57:15 +0600

    Hashed password

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,20 +7,12 @@ const bcrypt = require('bcryptjs')
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
+    
     // if (!name || !email || !password) {
     //     throw new BadRequestError("Please provide name, email and password");
     // }
 
-    //hash password
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const tempUser = {
-        name,
-        email,
-        password: hashedPassword,
-    };
-
-    const user = await User.create({ ...tempUser });
+    const user = await User.create({ ...req.body });
     res.status(StatusCodes.CREATED).send(user);
 }
 
