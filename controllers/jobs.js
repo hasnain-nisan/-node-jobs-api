5788266a3a834e4a21cc4bb03b28858f3f3ab019
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-05-25 17:00:49 +0600

    Job crud operatoion done

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,23 +1,82 @@
 
+const Job = require('../models/Job')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError, NotFoundError} = require('../errors')
+
 
 const getAllJobs = async (req, res) => {
-  res.send("Get all jobs User");
-};
+  const jobs = await Job.find({
+    createdBy: req.user.id
+  }).sort('createdAt')
+
+  res.status(StatusCodes.OK).send({
+    jobs: jobs,
+    count: jobs.length
+  })
+}
 
 const createJob = async (req, res) => {
-  res.send(req.user)
+  req.body.createdBy = req.user._id
+  const job = await Job.create({...req.body})
+  
+  res.status(StatusCodes.CREATED).send({
+    job: job
+  })
 };
 
 const getJob = async (req, res) => {
-  res.send("Get job");
+  const {id} = req.params
+  const {_id} = req.user
+  const job = await Job.findOne({
+    _id: id,
+    createdBy: _id
+  })
+
+  if(!job){
+    throw new NotFoundError(`No job found with the id:${id}`);
+  }
+
+  res.status(StatusCodes.OK).send({
+    job: job
+  });
 };
 
 const updateJob = async (req, res) => {
-  res.send("Update job");
+  const { id } = req.params;
+  const { _id } = req.user;
+  const {company, position, status } = req.body
+
+  if(company === "" || position === "" || status === ""){
+    throw new BadRequestError('Company, position or status must not be empty')
+  }
+
+  const job = await Job.findByIdAndUpdate(
+    { _id: id, createdBy: _id,},
+    {...req.body},
+    {new: true, runValidators: true}
+  );
+
+  if(!job){
+    throw new NotFoundError('Job not found')
+  }
+
+  res.status(StatusCodes.OK).send({ job: job });
 };
 
-const deleteJob = async (req, res) => {
-  res.send("Delete job");
+const deleteJob = async (req, res) => { 
+  const { id } = req.params;
+  const { _id } = req.user;
+
+  const job = await Job.findByIdAndRemove({
+    _id: id, 
+    createdBy: _id
+  })
+
+  if(!job) {
+    throw new NotFoundError('Job not found')
+  }
+
+  res.status(StatusCodes.OK).send()
 };
 
 module.exports = {
